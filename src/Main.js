4f718c3508677c9ceb5f37b2563d5a84b13e2cda
brunim1101 <brunormoreira@outlook.com>
2019-10-18 10:41:44 -0300

    pagina main layout semipronto

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logoIoasys from './imgs/logo-nav.png'
 import icLupa from './imgs/ic-search.svg'
+import icClose from './imgs/ic-close.svg'
 import './styles/Main.css'
 
 class Main extends Component {
@@ -13,30 +14,47 @@ class Main extends Component {
     }
 
     handleLupa(){
-        this.setState({isSearching: true})
+        if(this.state.isSearching){
+            this.setState({isSearching: false})
+        }else
+            this.setState({isSearching: true})
     }
 
     render(){
         return(
-            <div className="topo">
+            <div className="site">
+                <div className="topo">
 
-                {!this.state.isSearching ? (
-                    <>
-                        <div />
-                        
-                        <img src={logoIoasys} className="logoIoasys" alt="logo" />
-        
-                        <img src={icLupa} className="icLupa" alt="lupa" onClick={this.handleLupa.bind(this)} />
-                    </>
-                ) : (
-                    <>
-                        <div className="searchForm">
-                            <img src={icLupa} className="icLupaSearch" alt="lupa"  />
-                            <input id="search" type="text" placeholder="Pesquisar" />
-                        </div>
-                    </>
-                )}
+                    {!this.state.isSearching ? (
+                        <>
+                            <div />
+                            
+                            <img src={logoIoasys} className="logoIoasys" alt="logo" />
+            
+                            <img src={icLupa} className="icLupa" alt="lupa" onClick={this.handleLupa.bind(this)} />
+                        </>
+                    ) : (
+                        <>
+                            <div className="searchForm">
+                                <div className="searchInput">
+                                <img src={icLupa} className="icLupaSearch" alt="lupa"  />
+                                <input id="search" type="text" placeholder="Pesquisar" />
+                                </div>
+                                <img src={icClose} className="icClose" alt="fechar" onClick={this.handleLupa.bind(this)} />
+                            </div>
+                        </>
+                    )}
+                    
+                </div>
                 
+                <div className="body">
+                    {!this.state.isSearching ? (
+                        <p>Clique na busca para iniciar</p>
+                    ) : (
+                        null
+                    ) }
+                        
+                </div>
             </div>
         )
     }
